ebfde6540a7b52d982ef94b4162100eb70f335dc
Afina <afinawalton95@gmail.com>
2021-11-08 11:17:09 -0800

    Update Customer model's phone_num and create register_at column

--- app/models/customer.py
+++ app/models/customer.py
@@ -4,4 +4,14 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     postal_code = db.Column(db.String(5))
-    phone_num = db.Column(db.String(12))
+    phone_num = db.Column(db.String(14))
+    register_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "registered_at": self.register_at,
+            "postal_code": self.postal_code,
+            "phone": self.phone_num
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,27 @@
-from flask import Blueprint
-# CUSTOMER ROUTES #
+from flask import Blueprint, jsonify
+from flask.globals import request
+from app.models.customer import Customer
+from app import db
 
+# CUSTOMER ROUTES #
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 
 @customers_bp.route("", methods=["POST"])
 def create_goal():
+    request_body = request.get_json()
     pass
 
 def read_goals():
+    customers = Customer.query.all()
+    # Gives instance of customer
+
+    response_body = []
+
+    if not customers:
+        return jsonify([]), 200
+
+    for customer in customers:
+        response_body.append(customer)
     pass
 
 def read_one_goal():
