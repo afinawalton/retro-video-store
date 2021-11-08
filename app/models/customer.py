c4d68df95523b34cb17d899aa4b9d04052106bd4
Afina <afinawalton95@gmail.com>
2021-11-08 11:04:17 -0800

    Add customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,6 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String(5))
+    phone_num = db.Column(db.String(12))
