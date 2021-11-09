c06a09ce81cf52bc97dbf39e467e291ea77eadba
Rebeca <muniz.movement@gmail.com>
2021-11-09 11:53:39 -0800

    changes to model and routes

--- app/models/video.py
+++ app/models/video.py
@@ -2,3 +2,14 @@ from app import db
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.String)
+    inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "inventory": self.inventory
+        }
