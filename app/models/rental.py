8a5c15864960c42a3c72d4e45fee1af45d834481
Afina <afinawalton95@gmail.com>
2021-11-11 12:52:00 -0800

    Update customer and rental model

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,4 +1,7 @@
 from app import db
 
 class Rental(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "rentals"
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True, nullable=False)
+    due_date = db.Column(db.DateTime)
